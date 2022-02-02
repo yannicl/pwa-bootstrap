98a76f0d25e4c542f56d99571b92dd38ddc1fd75
Yassine063 <yas-elkhaddioui@hotmail.fr>
2022-02-02 16:52:35 -0500

    enlever les slach

--- sw.js
+++ sw.js
@@ -2,21 +2,21 @@ this.addEventListener('install', function(event) {
     event.waitUntil(
       caches.open('v1').then(function(cache) {
         return cache.addAll([
-         '/',
-          '/index.html',
-          '/index.js',
+         '',
+          'index.html',
+          'index.js',
           // '/favicon.ico',
-          '/mobile2.jpg',
-          '/ordinateur.png',
-          '/tablettes.jpg',
-          '/icon1.png',
-          '/icon-192x192.png',
-          '/icon-256x256.png',
-          '/icon-384x384.png',
-          '/icon-512x512.png',
-          '/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
-          '/bootstrap-5.1.3-dist/css/bootstrap.min.css',
-          '/icons-1.7.2/font/bootstrap-icons.css'
+          'mobile2.jpg',
+          'ordinateur.png',
+          'tablettes.jpg',
+          'icon1.png',
+          'icon-192x192.png',
+          'icon-256x256.png',
+          'icon-384x384.png',
+          'icon-512x512.png',
+          'bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
+          'bootstrap-5.1.3-dist/css/bootstrap.min.css',
+          'icons-1.7.2/font/bootstrap-icons.css'
           
             
         ]);
