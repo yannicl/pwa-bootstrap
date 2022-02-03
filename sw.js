70d1bf91be07ce40b70af25cc7e72b5a56ef3e8d
Yassine063 <yas-elkhaddioui@hotmail.fr>
2022-02-02 22:17:21 -0500

    ajout pwa-bootstrap

--- sw.js
+++ sw.js
@@ -3,21 +3,20 @@ this.addEventListener('install', function(event) {
       caches.open('v1').then(function(cache) {
         return cache.addAll([
 
-         '/',
-          'pwa-bootstarp/index.html',
-          'pwa-bootstarp/index.js',
-          // '/favicon.ico',
-          'pwa-bootstarp/mobile2.jpg',
-          'pwa-bootstarp/ordinateur.png',
-          'pwa-bootstarp/tablettes.jpg',
-          'pwa-bootstarp/icon1.png',
-          'pwa-bootstarp/icon-192x192.png',
-          'pwa-bootstarp/icon-256x256.png',
-          'pwa-bootstarp/icon-384x384.png',
-          'pwa-bootstarp/icon-512x512.png',
-          'pwa-bootstarp/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
-          'pwa-bootstarp/bootstrap-5.1.3-dist/css/bootstrap.min.css',
-          'pwa-bootstarp/icons-1.7.2/font/bootstrap-icons.css'
+      
+          '/pwa-bootstrap/index.html',
+          '/pwa-bootstrap/index.js',
+          '/pwa-bootstrap/mobile2.jpg',
+          '/pwa-bootstrap/ordinateur.png',
+          '/pwa-bootstrap/tablettes.jpg',
+          '/pwa-bootstrap/icon1.png',
+          '/pwa-bootstrap/icon-192x192.png',
+          '/pwa-bootstrap/icon-256x256.png',
+          '/pwa-bootstrap/icon-384x384.png',
+          '/pwa-bootstrap/icon-512x512.png',
+          '/pwa-bootstrap/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
+          '/pwa-bootstrap/bootstrap-5.1.3-dist/css/bootstrap.min.css',
+          '/pwa-bootstrap/icons-1.7.2/font/bootstrap-icons.css'
           
             
         ]);
